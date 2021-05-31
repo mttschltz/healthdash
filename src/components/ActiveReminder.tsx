532c6f18c6fde1dfb5189eb78c859b55b6760156
Matt Schultz <matt@mattschultz.info>
2021-05-31 21:30:27 +0930

    refactor: copy reminder components from old project

---
+++ src/components/ActiveReminder.tsx
@@ -0,0 +1,221 @@
+import {
+  Box,
+  Checkbox,
+  Flex,
+  Heading,
+  Spacer,
+  Stack,
+  Text,
+} from '@chakra-ui/react'
+import { Reminder, Todo } from '@/models/models'
+import React, {
+  FunctionComponent,
+  useState,
+  useEffect,
+  useCallback,
+} from 'react'
+
+type ReminderStatus = 'pending' | 'overdue'
+
+const calcMinutesRemaining = (d1: Date, d2: Date | null) =>
+  d2 === null ? -1 : Math.ceil((d2.getTime() - d1.getTime()) / 60000)
+
+export interface ActiveReminderManagerProps {
+  reminder: Reminder
+  completeTodo: (tn: string) => void
+  uncompleteTodo: (tn: string) => void
+  completeChildTodo: (ctn: string) => void
+  uncompleteChildTodo: (ctn: string) => void
+}
+
+interface ActiveReminderProps {
+  name: string
+  interval: number
+  todos: Todo[]
+
+  completed: number
+  nextDue: Date | null
+
+  minRemaining: number | null
+  status: ReminderStatus
+  parentMinRemaining?: number | null
+  parentName?: string
+  updateTodoCallback: (tn: string, checked: boolean) => void
+}
+
+const ActiveReminder: FunctionComponent<ActiveReminderProps> = ({
+  name,
+  todos,
+  status,
+  minRemaining,
+  parentName,
+  parentMinRemaining,
+  updateTodoCallback,
+}) => (
+  <Box
+    spacing={2}
+    p={4}
+    bgColor={status === `pending` ? `lightgreen` : `lightpink`}
+  >
+    <Stack>
+      {parentName && (
+        <Flex>
+          <Heading size="sm">{parentName}</Heading>
+          <Spacer />
+          <Heading size="sm">{parentMinRemaining}m left</Heading>
+        </Flex>
+      )}
+      <Flex>
+        <Heading size="lg">{name}</Heading>
+        <Spacer />
+        {minRemaining && (
+          <Heading size="lg">
+            {status === `overdue` ? `Due` : `${minRemaining}m left`}
+          </Heading>
+        )}
+      </Flex>
+      <Spacer />
+      {todos.map((t, i) =>
+        status === `overdue` ? (
+          <Checkbox
+            // disable this rule as there's nothing inherently unique about todos
+            // eslint-disable-next-line react/no-array-index-key
+            key={i}
+            size="lg"
+            onChange={(e) => {
+              updateTodoCallback(t.name, e.target.checked)
+            }}
+            colorScheme="red"
+          >
+            {t.name}
+          </Checkbox>
+        ) : (
+          // disable this rule as there's nothing inherently unique about todos
+          // eslint-disable-next-line react/no-array-index-key
+          <Text fontSize="lg" key={i}>
+            {t.name}
+          </Text>
+        ),
+      )}
+    </Stack>
+  </Box>
+)
+
+const shouldParentOverrideChild = (
+  parentReminder: Reminder,
+  childReminder: Reminder | null,
+) =>
+  childReminder === null || // no child
+  // parent has less remaining, with 1 min buffer
+  // use times not minRemaining as rounding causes problems
+  (parentReminder.nextDue !== null &&
+    childReminder.nextDue !== null &&
+    parentReminder.nextDue <=
+      new Date(childReminder.nextDue.getTime() + 60000)) ||
+  // parent is overdue
+  (parentReminder.nextDue !== null && parentReminder.nextDue <= new Date())
+
+export const ActiveReminderManager: FunctionComponent<ActiveReminderManagerProps> =
+  ({
+    completeTodo,
+    uncompleteTodo,
+    completeChildTodo,
+    uncompleteChildTodo,
+    reminder: parentReminder,
+  }) => {
+    const childReminder = parentReminder.child
+    const [minRemainingChild, setMinRemainingChild] =
+      useState<number | null>(null)
+    const [minRemainingParent, setMinRemainingParent] =
+      useState<number | null>(null)
+    const [childIsActive, setChildIsActive] = useState(false)
+    const [status, setStatus] = useState<ReminderStatus>(`pending`)
+
+    const updateTodoCallback = useCallback(
+      (tn: string, checked: boolean) => {
+        if (checked) {
+          completeTodo(tn)
+        } else {
+          uncompleteTodo(tn)
+        }
+      },
+      [completeTodo, uncompleteTodo],
+    )
+
+    const updateChildTodoCallback = useCallback(
+      (tn: string, checked: boolean) => {
+        if (checked) {
+          completeChildTodo(tn)
+        } else {
+          uncompleteChildTodo(tn)
+        }
+      },
+      [completeChildTodo, uncompleteChildTodo],
+    )
+
+    useEffect(() => {
+      const update = () => {
+        if (
+          shouldParentOverrideChild(parentReminder, childReminder) ||
+          childReminder === null
+        ) {
+          const newMinRemainingParent = calcMinutesRemaining(
+            new Date(),
+            parentReminder.nextDue,
+          )
+          setChildIsActive(false)
+          setStatus(newMinRemainingParent <= 0 ? `overdue` : `pending`)
+          setMinRemainingParent(newMinRemainingParent)
+          const newMinRemainingChild = calcMinutesRemaining(
+            new Date(),
+            childReminder?.nextDue || null,
+          )
+          setMinRemainingChild(newMinRemainingChild)
+        } else {
+          const newMinRemainingChild = calcMinutesRemaining(
+            new Date(),
+            childReminder.nextDue,
+          )
+          setChildIsActive(true)
+          setStatus(newMinRemainingChild <= 0 ? `overdue` : `pending`)
+          setMinRemainingChild(newMinRemainingChild)
+          const newMinRemainingParent = calcMinutesRemaining(
+            new Date(),
+            parentReminder.nextDue,
+          )
+          setMinRemainingParent(newMinRemainingParent)
+        }
+      }
+      update()
+
+      const interval = setInterval(update, 2000)
+
+      return () => clearInterval(interval)
+    }, [childReminder, parentReminder])
+
+    return childIsActive && childReminder ? (
+      <ActiveReminder
+        completed={childReminder.completed}
+        interval={childReminder.interval}
+        minRemaining={minRemainingChild}
+        name={childReminder.name}
+        nextDue={childReminder.nextDue}
+        status={status}
+        todos={childReminder.todos}
+        updateTodoCallback={updateChildTodoCallback}
+        parentMinRemaining={minRemainingParent}
+        parentName={parentReminder.name}
+      />
+    ) : (
+      <ActiveReminder
+        completed={parentReminder.completed}
+        interval={parentReminder.interval}
+        minRemaining={minRemainingParent}
+        name={parentReminder.name}
+        nextDue={parentReminder.nextDue}
+        status={status}
+        todos={parentReminder.todos}
+        updateTodoCallback={updateTodoCallback}
+      />
+    )
+  }
---
+++ src/components/ReminderConfig.tsx
@@ -0,0 +1,279 @@
+import {
+  Stack,
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+  Heading,
+  Box,
+  Button,
+} from '@chakra-ui/react'
+import {
+  Form,
+  Formik,
+  Field,
+  useFormikContext,
+  FieldArray,
+  getIn,
+} from 'formik'
+import debounce from 'just-debounce-it'
+import React, { FunctionComponent, useCallback } from 'react'
+
+interface TodosProps {
+  fieldName: string
+  todos: string[]
+  todoErrors: string | string[] | undefined
+}
+const Todos: FunctionComponent<TodosProps> = ({
+  fieldName,
+  todos,
+  todoErrors,
+}) => (
+  <FieldArray
+    name={fieldName}
+    render={(arrayHelpers) => (
+      <div>
+        {todos && todos.length > 0 ? (
+          todos.map((todo, index) => (
+            // disable this rule as there's nothing inherently unique about todos
+            // eslint-disable-next-line react/no-array-index-key
+            <div key={index}>
+              <Field name={`${fieldName}.${index}`} />
+              <button
+                type="button"
+                onClick={() => arrayHelpers.remove(index)} // remove a todo from the list
+              >
+                [ - ]
+              </button>
+              &nbsp;&nbsp;
+              <button
+                type="button"
+                onClick={() => arrayHelpers.insert(index, ``)} // insert an empty string at a position
+              >
+                [ + ]
+              </button>
+            </div>
+          ))
+        ) : (
+          <button type="button" onClick={() => arrayHelpers.push(``)}>
+            {/* show this when user has removed all todos from the list */}
+            Add
+          </button>
+        )}
+        {/* TODO: Update this and above to use chakra forms */}
+        <Box>{todoErrors}</Box>
+      </div>
+    )}
+  />
+)
+
+interface ReminderConfigProps {
+  initialValues: Partial<ReminderConfigValues>
+  onUpdate: (values: ReminderConfigValues, valid: boolean) => void
+}
+
+export interface ReminderConfigValues {
+  name: string
+  interval: number
+  todos: string[]
+  child?: ReminderConfigValues
+}
+
+export interface ReminderConfigErrors {
+  name?: string
+  interval?: string
+  todos?: string
+  child?: ReminderConfigErrors
+}
+
+const AutoSave = ({ debounceMs }) => {
+  const formik = useFormikContext()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const debouncedSubmit = useCallback(
+    debounce(() => formik.submitForm(), debounceMs),
+    [debounceMs, formik.submitForm, debounce],
+  )
+
+  React.useEffect(() => {
+    debouncedSubmit()
+  }, [debouncedSubmit, formik.values])
+
+  return null
+}
+
+const validateReminderValues =
+  (onUpdate: (values: ReminderConfigValues, valid: boolean) => void) =>
+  (values: ReminderConfigValues) => {
+    const errors: ReminderConfigErrors = {}
+    let hasErrors = false
+
+    // Parent
+    if (!values.name) {
+      errors.name = `Required`
+      hasErrors = true
+    }
+    if (!values.interval) {
+      errors.interval = `Required`
+      hasErrors = true
+    }
+    if (values.todos.length === 0 || !values.todos?.some((t) => t !== ``)) {
+      errors.todos = `At least 1 Todo is required`
+      hasErrors = true
+    }
+
+    // Child
+    if (values.child) {
+      if (!values.child.name) {
+        errors.child = errors.child || {}
+        errors.child.name = `Required`
+        hasErrors = true
+      }
+
+      if (!values.child.interval) {
+        errors.child = errors.child || {}
+        errors.child.interval = `Required`
+        hasErrors = true
+      }
+      if (
+        values.child.todos.length === 0 ||
+        !values.child.todos?.some((t) => t !== ``)
+      ) {
+        errors.child = errors.child || {}
+        errors.child.todos = `At least 1 Todo is required`
+        hasErrors = true
+      }
+    }
+
+    if (hasErrors) {
+      onUpdate({ name: ``, interval: 0, todos: [] }, false)
+    }
+    return errors
+  }
+
+export const ReminderConfig: FunctionComponent<ReminderConfigProps> = ({
+  initialValues,
+  onUpdate,
+}) => (
+  <Formik
+    initialValues={{
+      name: initialValues.name || ``,
+      interval: initialValues.interval || -1,
+      todos: initialValues.todos || [],
+      child: initialValues.child,
+    }}
+    validate={validateReminderValues(onUpdate)}
+    onSubmit={(values) =>
+      new Promise((resolve) =>
+        setTimeout(() => {
+          onUpdate(
+            {
+              ...values,
+              interval:
+                typeof values.interval === `number`
+                  ? values.interval
+                  : parseInt(values.interval, 10),
+              child: !values.child
+                ? undefined
+                : { ...values.child, interval: values.child.interval },
+            },
+            true,
+          )
+          resolve(null)
+        }, 500),
+      )
+    }
+    render={({ values, errors, setFieldValue }) => (
+      <Form>
+        <AutoSave debounceMs={200} />
+        <Stack spacing={4} maxW="md" bgColor="lightgray" p={2}>
+          <Field name="name">
+            {({ field, form }) => (
+              <FormControl isInvalid={form.errors.name && form.touched.name}>
+                <FormLabel htmlFor="name">Name</FormLabel>
+                <Input {...field} id="name" placeholder="name" />
+                <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+              </FormControl>
+            )}
+          </Field>
+          <Field name="interval">
+            {({ field, form }) => (
+              <FormControl
+                isInvalid={form.errors.interval && form.touched.interval}
+              >
+                <FormLabel htmlFor="interval">Interval</FormLabel>
+                <Input {...field} id="interval" placeholder="" />
+                <FormErrorMessage>{form.errors.interval}</FormErrorMessage>
+              </FormControl>
+            )}
+          </Field>
+          <Heading size="sm">Todos</Heading>
+          <Todos
+            fieldName="todos"
+            todos={values.todos}
+            todoErrors={errors.todos}
+          />
+          {/* Child */}
+          {!values.child ? (
+            <Button
+              onClick={() =>
+                setFieldValue(`child`, {
+                  interval: 2,
+                  name: `Micro break`,
+                  todos: [`Stretch`, `Drink water`],
+                })
+              }
+            >
+              Add Intermediate Reminder
+            </Button>
+          ) : (
+            <>
+              <Heading size="sm">Intermediate Reminder</Heading>
+              <Field name="child.name">
+                {({ field, form }) => (
+                  <FormControl
+                    isInvalid={
+                      form.errors.child?.name && form.touched.child.name
+                    }
+                  >
+                    <FormLabel htmlFor="child-name">Name</FormLabel>
+                    <Input
+                      {...field}
+                      id="child-name"
+                      placeholder="child-name"
+                    />
+                    <FormErrorMessage>
+                      {form.errors.child?.name}
+                    </FormErrorMessage>
+                  </FormControl>
+                )}
+              </Field>
+              <Field name="child.interval">
+                {({ field, form }) => (
+                  <FormControl
+                    isInvalid={
+                      form.errors.child?.interval && form.touched.child.interval
+                    }
+                  >
+                    <FormLabel htmlFor="child-interval">Interval</FormLabel>
+                    <Input {...field} id="child-interval" placeholder="" />
+                    <FormErrorMessage>
+                      {form.errors.child?.interval}
+                    </FormErrorMessage>
+                  </FormControl>
+                )}
+              </Field>
+              <Todos
+                fieldName="child.todos"
+                todos={values.child.todos}
+                todoErrors={getIn(errors.child, `todos`)}
+              />
+              <Button onClick={() => setFieldValue(`child`, undefined)}>
+                remove Intermediate Reminder
+              </Button>
+            </>
+          )}
+        </Stack>
+      </Form>
+    )}
+  />
+)
